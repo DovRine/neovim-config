1b4dc9c398f14c966fcc45a140fcdbb69fd490aa
Dov Rine <dov.rine@gmail.com>
2023-10-14 12:37:01 -0700

    initial commit

---
+++ lua/config/lazy.lua
@@ -0,0 +1,25 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+	},
+	-- { "folke/which-key.nvim" },
+})
+
