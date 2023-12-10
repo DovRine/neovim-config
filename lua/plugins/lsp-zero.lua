368e887aa49adb8472b496a91ad4ef9d59c7da05
Dov Rine <dov.rine@gmail.com>
2023-12-09 22:37:17 -0800

    update deps

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -8,7 +8,7 @@ return {
             { 'neovim/nvim-lspconfig' },
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/nvim-cmp' },
-            { 'L3MON4D3/LuaSnip' },
+            -- { 'L3MON4D3/LuaSnip' },
         },
         config = function()
             local lsp = require('lsp-zero')
