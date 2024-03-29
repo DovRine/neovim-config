7ace0a33b7b7785b0bfc99cb2dd69ce79c3f9774
Dov Rine <dov.rine@gmail.com>
2024-03-28 20:39:52 -0700

    add harpoon remove keymap

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -35,21 +35,23 @@ return {
             vim.keymap.set("n", "<leader>hh", function() toggle_telescope(harpoon:list()) end,
                 { desc = "Open harpoon window" })
             vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end,
-                { desc = "append" })
+                { desc = "append to harpoon" })
+            vim.keymap.set("n", "<leader>hd", function() harpoon:list():remove() end,
+                { desc = "remove from harpoon" })
             vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-                { desc = "toggle quick menu" })
+                { desc = "toggle harpoon quick menu" })
             vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(1) end,
-                { desc = "select 1" })
+                { desc = "select harpoon 1" })
             vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(2) end,
-                { desc = "select 2" })
+                { desc = "select harpoon 2" })
             vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(3) end,
-                { desc = "select 3" })
+                { desc = "select harpoon 3" })
             vim.keymap.set("n", "<leader>h;", function() harpoon:list():select(4) end,
-                { desc = "select 4" })
+                { desc = "select harpoon 4" })
             vim.keymap.set("n", "<leader>hn", function() harpoon:list():prev() end,
-                { desc = "previous" })
+                { desc = "harpoon previous" })
             vim.keymap.set("n", "<leader>hm", function() harpoon:list():next() end,
-                { desc = "next" })
+                { desc = "harpoon next" })
         end,
     }
 }
